4ccf81ed95f92e4785320c0e084bc65edffaa05d
Joshua Jeth F Manalili <shemerlo123@gmail.com>
2024-03-04 09:26:18 +0800

    commit

---
+++ register.js
@@ -0,0 +1,16 @@
+console.log("Hi Gar!");
+
+let btnRegister = document.querySelector("#btnRegister");
+let txtUsername = document.querySelector("#txtUsername");
+let txtPassword = document.querySelector("#txtPassword");
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword");
+let txtAge = document.querySelector("#txtAge");
+
+btnRegister.onclick = function(){
+  register(txtUsername.value, txtPassword.value, txtConfirmPassword.value, txtAge.value);
+}
+
+function register (username, password, confirmPassword, age ){
+    console.log(username, password, confirmPassword, age);
+        // alert("Register button has been clicked")
+}
